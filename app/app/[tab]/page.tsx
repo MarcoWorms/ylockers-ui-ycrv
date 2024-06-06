f015e88d1800de821ac731b41eeed3257170d017
Marco Guaspari Worms <marcogworms@gmail.com>
2024-06-06 08:40:23 -0300

    move component

--- app/app/[tab]/page.tsx
+++ app/app/[tab]/page.tsx
@@ -192,7 +192,6 @@ export default function Home() {
                   </div>
                 </>
               )}
-              
             </div>
           </div>
           <div className="mt-8">
