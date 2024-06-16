d18b14e98715fbe5b2956f6be27d6f13f8be1a28
Marco Worms <marcogworms@gmail.com>
2024-06-15 23:53:43 -0300

    update provider to use tenderly testnet

--- app/contexts/Provider.tsx
+++ app/contexts/Provider.tsx
@@ -18,20 +18,20 @@ import env from '@/lib/env';
 
 const queryClient = new QueryClient();
 
-// const newMainnet = Object.assign({}, mainnet, {
-//   "id": 420420,
-//   "rpcUrls": {
-//     "default": {
-//       "http": ["https://virtual.mainnet.rpc.tenderly.co/f27fc283-2742-40d2-97cc-ffe73e9f5787"]
-//     }
-//   }
-// })
+const newMainnet = Object.assign({}, mainnet, {
+  "id": 420420,
+  "rpcUrls": {
+    "default": {
+      "http": ["https://virtual.mainnet.rpc.tenderly.co/f27fc283-2742-40d2-97cc-ffe73e9f5787"]
+    }
+  }
+})
 
 const config = getDefaultConfig({
   appName: 'yPrisma',
   projectId: '84801a4fb569adb34f184f543b6d1762',
-  // chains: [newMainnet],
-  chains: [mainnet],
+  chains: [newMainnet],
+  // chains: [mainnet],
   transports: {
     [mainnet.id]: http(process.env.NEXT_PUBLIC_RPC_1)
   },
@@ -47,7 +47,7 @@ const config = getDefaultConfig({
       safeWallet
     ]
   }],
-  ssr: true, // If your dApp uses server side rendering (SSR)
+  ssr: true,
 });
 
 export default function Provider ({
