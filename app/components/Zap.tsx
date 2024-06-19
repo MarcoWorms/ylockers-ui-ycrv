2443ce9576c23d6fcbbabbb4c098393ae5117442
Marco Worms <marcogworms@gmail.com>
2024-06-19 18:37:04 -0300

    fix import

--- app/components/Zap.tsx
+++ app/components/Zap.tsx
@@ -7,20 +7,7 @@ import abis from '@/app/abis';
 import Image from 'next/image';
 import { erc20Abi } from 'viem';
 import bmath from '@/lib/bmath';
-import useBalances from '../../hooks/useBalances';
-
-const INPUT_TOKENS = [
-  { address: '0xc5bDdf9843308380375a611c18B50Fb9341f502A', symbol: 'yveCRV-DAO' },
-  { address: '0x9d409a0A012CFbA9B15F6D4B36Ac57A46966Ab9a', symbol: 'yvBOOST' },
-  { address: '0xFCc5c47bE19d06BF83eB04298b026F81069ff65b', symbol: 'yCRV' },
-  { address: '0x27B5739e22ad9033bcBf192059122d163b60349D', symbol: 'yvyCRV' },
-  { address: '0x6E9455D109202b426169F0d8f01A3332DAE160f3', symbol: 'lp-yCRVv2' },
-  { address: '0x453D92C7d4263201C69aACfaf589Ed14202d83a4', symbol: 'yCRV-f v1' },
-  { address: '0x99f5aCc8EC2Da2BC0771c32814EFF52b712de1E5', symbol: 'yCRV-f v2' },
-  { address: '0xD533a949740bb3306d119CC777fa900bA034cd52', symbol: 'CRV' },
-  { address: '0xc97232527B62eFb0D8ed38CF3EA103A6CcA4037e', symbol: 'lp-yCRVv1' },
-  { address: '0xE9A115b77A1057C918F997c32663FdcE24FB873f', symbol: 'YBS' },
-];
+import useBalances from '@/hooks/useBalances';
 
 const OUTPUT_TOKENS = [
   { address: '0xFCc5c47bE19d06BF83eB04298b026F81069ff65b', symbol: 'yCRV' },
